c1aad85956155600bea506959cdbf4da51b8482c
Alexander <alexanderc@outlook.com>
2017-05-09 09:30:31 -0500

    Cambio en versión de la pagina

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Movies UPTC' });
+  res.render('index', { title: 'Movies UPTC - v2' });
 });
 
 module.exports = router;
